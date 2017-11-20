136f6c888637eac78eaf3d81120f7b7669f7c349
tysky <dtysky51@gmail.com>
2017-11-20 22:03:55 +0300

    add dotenv and debug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Koa from 'koa';
 import Rollbar from 'rollbar';
+import dotenv from 'dotenv';
+// import container from '../container';
+
+dotenv.config();
 
 const app = new Koa();
 const rollbar = new Rollbar(process.env.ROLLBAR_TOKEN);
 
-rollbar.log('Hello world!');
+// rollbar.log('Hello world!');
 
 app.use(async (ctx, next) => {
   try {
@@ -15,7 +19,7 @@ app.use(async (ctx, next) => {
 });
 
 app.use((ctx) => {
-  ctx.body = 'Hello User!!!';
+  ctx.body = 'Hello User!';
 });
 
 export default app;
