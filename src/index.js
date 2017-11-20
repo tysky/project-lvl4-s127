6a21c20ef0aadb99d33d997aa18cb45d4ab21ade
tysky <dtysky51@gmail.com>
2017-11-20 21:34:31 +0300

    add rollbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import Koa from 'koa';
+import Rollbar from 'rollbar';
 
 const app = new Koa();
+const rollbar = new Rollbar(process.env.ROLLBAR_TOKEN);
+
+rollbar.log('Hello world!');
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    rollbar.error(err, ctx.request);
+  }
+});
 
 app.use((ctx) => {
   ctx.body = 'Hello User!!!';
