99a10765782e0fdb97ccd66374abe23f6b6a75f3
tysky <dtysky51@gmail.com>
2017-11-22 22:46:41 +0300

    return rollback

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import Koa from 'koa';
-// import Rollbar from 'rollbar';
+import Rollbar from 'rollbar';
 import dotenv from 'dotenv';
 // import container from '../container';
 
 dotenv.config();
 
+const rollbar = new Rollbar(process.env.ROLLBAR_TOKEN);
 const app = new Koa();
-// const rollbar = new Rollbar(process.env.ROLLBAR_TOKEN);
 
-// rollbar.log('Hello world!');
+rollbar.log('Hello new world!');
 
-// app.use(async (ctx, next) => {
-//   try {
-//     await next();
-//   } catch (err) {
-//     rollbar.error(err, ctx.request);
-//   }
-// });
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    rollbar.error(err, ctx.request);
+  }
+});
 
 app.use((ctx) => {
   ctx.body = 'Hello User!';
