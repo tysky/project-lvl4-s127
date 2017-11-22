d91e08fbf3fb4b7f2daf7af4b867cb2384467fb8
tysky <dtysky51@gmail.com>
2017-11-22 21:19:27 +0300

    rollbar off

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import Koa from 'koa';
-import Rollbar from 'rollbar';
+// import Rollbar from 'rollbar';
 import dotenv from 'dotenv';
 // import container from '../container';
 
 dotenv.config();
 
 const app = new Koa();
-const rollbar = new Rollbar(process.env.ROLLBAR_TOKEN);
+// const rollbar = new Rollbar(process.env.ROLLBAR_TOKEN);
 
 // rollbar.log('Hello world!');
 
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    rollbar.error(err, ctx.request);
-  }
-});
+// app.use(async (ctx, next) => {
+//   try {
+//     await next();
+//   } catch (err) {
+//     rollbar.error(err, ctx.request);
+//   }
+// });
 
 app.use((ctx) => {
   ctx.body = 'Hello User!';
